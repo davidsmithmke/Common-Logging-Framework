0fe5f7e77162d428c1e6acd77123c76d4f8e962c
David Smith <dsmith@davidsmithmke.com>
2019-04-14 19:12:48 -0500

    Add NLog Ninject Bindings

---
+++ NLog Ninject Bindings/NLogModule.cs
@@ -0,0 +1,15 @@
+﻿using Ninject.Modules;
+using Washable.Logging.Common;
+using Washable.Logging.NLogFramework;
+
+namespace Washable.Logging.NLog.Ninject
+{
+    public class NLogModule
+        : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ILogger>().ToMethod(context => new NLogLogger(name: context.Request.Target != null ? context.Request.Target.Member.DeclaringType.FullName : System.Reflection.Assembly.GetExecutingAssembly().GetName().FullName));
+        }
+    }
+}
