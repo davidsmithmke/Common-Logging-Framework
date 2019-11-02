ad683182a4f390889cbf362ff9ad40f34ad4de15
David Smith <dsmith@davidsmithmke.com>
2019-11-02 08:40:42 -0500

    Add logger provider bindings

--- NLog Ninject Bindings/NLogModule.cs
+++ NLog Ninject Bindings/NLogModule.cs
@@ -10,6 +10,7 @@ namespace Washable.Logging.NLog.Ninject
         public override void Load()
         {
             Bind<ILogger>().ToMethod(context => new NLogLogger(name: context.Request.Target != null ? context.Request.Target.Member.DeclaringType.FullName : System.Reflection.Assembly.GetExecutingAssembly().GetName().FullName));
+            Bind<ILoggerProvider>().To<NLogLoggerProvider>().InSingletonScope();
         }
     }
 }
