de2c6b1f1868baa01b93703b625fd7ff995e3d4a
David Smith <dsmith@davidsmithmke.com>
2021-03-07 12:11:34 -0600

    Add Autofac Bindings Module

---
+++ NLog Autofac Bindings/NLogModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using Washable.Logging.Common;
+using Washable.Logging.NLogFramework;
+
+namespace Washable.Logging.NLog.Ninject
+{
+    public class NLogModule
+         : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<NLogLoggerProvider>().As<ILoggerProvider>();
+        }
+    }
+}
